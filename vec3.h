57e89126d762ef42d4e5dd8b449838902f1f466e
Martin Pettersson <kontakt@martinpettersson.se>
2019-05-13 22:47:51 +0200

    Make unit vector

--- vec3.h
+++ vec3.h
@@ -48,3 +48,8 @@ inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
     os << t.e[0] << " " << t.e[1] << " " << t.e[2];
     return os;
 }
+
+inline void vec3::make_unit_vector() {
+    float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2] * e[2]);
+    e[0] *= k; e[1] *= k; e[2] *= k;
+}
