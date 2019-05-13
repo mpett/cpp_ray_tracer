7ee93fdecf6c8e9dad452e366e2622b1d44d4ee4
Martin Pettersson <kontakt@martinpettersson.se>
2019-05-13 22:40:29 +0200

    Stream operators

--- vec3.h
+++ vec3.h
@@ -38,3 +38,13 @@ class vec3 {
 
     float e[3];
 };
+
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+    return os;
+}
