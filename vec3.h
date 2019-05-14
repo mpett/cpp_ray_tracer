c8c44bc722056e7f31e6f6ad6a404efb94a98c7b
Martin Pettersson <kontakt@martinpettersson.se>
2019-05-14 20:03:20 +0200

    dot operator

--- vec3.h
+++ vec3.h
@@ -81,3 +81,7 @@ inline vec3 operator/(vec3 v, float t) {
 inline vec3 operator*(const vec3 &v, float t) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
