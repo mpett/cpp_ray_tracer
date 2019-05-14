fb6b5a145e5e3a78ac6d8f3367844fff148a2117
Martin Pettersson <kontakt@martinpettersson.se>
2019-05-14 19:16:59 +0200

    + and - operators in vector

--- vec3.h
+++ vec3.h
@@ -53,3 +53,11 @@ inline void vec3::make_unit_vector() {
     float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2] * e[2]);
     e[0] *= k; e[1] *= k; e[2] *= k;
 }
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
