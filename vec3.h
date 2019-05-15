27a71f25118cf8dae7577bdd1340d6c06d43b7aa
Martin Pettersson <kontakt@martinpettersson.se>
2019-05-15 22:17:44 +0200

    += operator

--- vec3.h
+++ vec3.h
@@ -91,3 +91,10 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     (-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])), 
     (v1.e[0]*v2.e[1] - v1.e[1] * v2.e[0]));
 }
+
+inline vec3& vec3::operator+=(const vec3 &v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+}
