483b5f30c3731302bfea420ad2c5b70cd026c3e6
Martin Pettersson <kontakt@martinpettersson.se>
2019-05-30 17:54:13 +0200

    Readme update

--- ray_tracer.cpp
+++ ray_tracer.cpp
@@ -3,14 +3,7 @@
 #include "hitable_list.h"
 #include "float.h"
 #include "camera.h"
-
-vec3 random_in_unit_sphere() {
-    vec3 p;
-    do {
-        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
-    } while (p.squared_length() >= 1.0);
-    return p;
-}
+#include "material.h"
 
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
