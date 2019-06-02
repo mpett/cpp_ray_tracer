869eabc30337e2ebd2b0d5bdff2aabcd3aef4f46
Martin Pettersson <kontakt@martinpettersson.se>
2019-06-02 21:41:34 +0200

    Schlick polynomial approximation for glass reflectivity.

--- material.h
+++ material.h
@@ -16,7 +16,7 @@ vec3 random_in_unit_sphere() {
     do {
         p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
     } while (p.squared_length() >= 1.0);
-    
+
     return p;
 }
 
@@ -31,6 +31,12 @@ bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
     } else {
         return false;
     }
+}
+
+float schlick(float cosine, float ref_idx) {
+    float r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
 }
 
 class material {
