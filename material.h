594906695504d3616aedf9d9b48716cbd5f745d2
Martin Pettersson <kontakt@martinpettersson.se>
2019-06-03 22:49:48 +0200

    Changed sample size

--- material.h
+++ material.h
@@ -90,20 +90,30 @@ class dielectric : public material {
                 float ni_over_nt;
                 attenuation = vec3(1.0, 1.0, 0.0);
                 vec3 refracted;
+                float reflect_prob;
+                float cosine;
                 
                 if (dot(r_in.direction(), rec.normal) > 0) {
                     outward_normal = -rec.normal;
                     ni_over_nt = ref_idx;
+                    cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
                 } else {
                     outward_normal = rec.normal;
                     ni_over_nt = 1.0 / ref_idx;
+                    cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
                 }
 
                 if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
-                    scattered = ray(rec.p, refracted);
+                    reflect_prob = schlick(cosine, ref_idx);
                 } else {
                     scattered = ray(rec.p, reflected);
-                    return false;
+                    reflect_prob = 1.0;
+                }
+
+                if (drand48() < reflect_prob) {
+                    scattered = ray(rec.p, reflected);
+                } else {
+                    scattered = ray(rec.p, refracted);
                 }
 
                 return true;
