355946c2a6d85e860f282b0a58f44d8c5846fbe9
Martin Pettersson <kontakt@martinpettersson.se>
2019-06-09 19:06:11 +0200

    Color change for lambert sphere

--- ray_tracer.cpp
+++ ray_tracer.cpp
@@ -28,7 +28,7 @@ int main() {
     int ns = 50;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     hitable *list[5];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
     list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
