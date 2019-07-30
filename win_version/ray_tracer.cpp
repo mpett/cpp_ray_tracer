265b3f43fa926fa5abe30998c47fb6a62ebc6c72
unknown <martin@tivala.se>
2019-07-30 14:12:26 +0100

    Basic diffuse material.

--- win_version/ray_tracer.cpp
+++ win_version/ray_tracer.cpp
@@ -7,6 +7,26 @@
 #include "float.h"
 #include "camera.h"
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0 * vec3((rand() / (RAND_MAX + 1.0)), (rand() / (RAND_MAX + 1.0)), (rand() / (RAND_MAX + 1.0))) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
+vec3 color(const ray& r, hitable *world) {
+    hit_record rec;
+    if (world -> hit(r, 0.0, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color( ray(rec.p, target-rec.p), world );
+    } else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+    }
+}
+
 float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
@@ -17,17 +37,6 @@ float hit_sphere(const vec3& center, float radius, const ray& r) {
         return -1.0;
     } else {
         return (-b - sqrt(discriminant) ) / (2.0 * a);
-    }
-}
-
-vec3 color(const ray& r, hitable *world) {
-    hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
-    } else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0-t) * vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
 }
 
