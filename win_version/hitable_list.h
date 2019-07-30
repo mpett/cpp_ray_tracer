828a12e08e6fbc58bf40b35211658ed4b051a2a0
unknown <martin@tivala.se>
2019-07-30 08:16:10 +0100

    Updated main function.

--- win_version/hitable_list.h
+++ win_version/hitable_list.h
@@ -1,5 +1,6 @@
 #ifndef HITABLELISTH
 #define HITABLELISTH
+#define MAXFLOAT FLT_MAX
 
 #include "hitable.h"
 
@@ -23,7 +24,7 @@ bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) 
     double closest_so_far = t_max;
     
     for (int i = 0; i < list_size; i++) {
-        if (list[i] -> (r, t_min, closest_so_far, temp_rec)) {
+        if (list[i] -> hit(r, t_min, closest_so_far, temp_rec)) {
             hit_anything = true;
             closest_so_far = temp_rec.t;
             rec = temp_rec;
