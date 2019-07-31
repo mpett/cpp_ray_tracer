b51ae6f218ee9ddbba0ec0cb4672142f7853be09
unknown <martin@tivala.se>
2019-07-31 11:24:41 +0100

    Metal material

--- win_version/material.h
+++ win_version/material.h
@@ -1,3 +1,7 @@
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v, n) * n;
+}
+
 class material {
     public:
         virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
@@ -16,3 +20,17 @@ class lambertian : public material {
 
         vec3 albedo;
 };
+
+class metal : public material {
+    public:
+        metal(const vec3& a) : albedo(a) {}
+
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal) > 0);
+        }
+
+        vec3 albedo;
+}
